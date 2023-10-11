ae5e7837a21e5380ef5fcef4d6251c9c3bcb951e
EdmondKoko <107623627+EdmondKoko@users.noreply.github.com>
2023-10-11 14:54:45 +0300

    Update user_filters.py

--- yatube/core/templatetags/user_filters.py
+++ yatube/core/templatetags/user_filters.py
@@ -5,4 +5,5 @@ register = template.Library()
 
 @register.filter
 def addclass(field, css):
+    """Фильтр addclass, позволяющий добавлять CSS-класс к тегу шаблона."""
     return field.as_widget(attrs={'class': css})
