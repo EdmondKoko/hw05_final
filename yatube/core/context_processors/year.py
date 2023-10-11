58e6e56f21b3184650b127839227882c61939cfc
EdmondKoko <107623627+EdmondKoko@users.noreply.github.com>
2023-10-11 14:54:09 +0300

    Update year.py

--- yatube/core/context_processors/year.py
+++ yatube/core/context_processors/year.py
@@ -2,6 +2,7 @@ import datetime
 
 
 def year(request):
+    """Контекст-процессор, добавляющий текущий год на все страницы в переменную {{ year }}."""
     return {
         'year': datetime.datetime.now().year
     }
